5f44088e0139b958210d8b283e6c38913bf4d1d2
arley <34805850@qq.com>
2018-09-28 14:09:02 +0800

    Site updated: 2018-09-28 14:09:01

--- about/script/main.js
+++ about/script/main.js
@@ -1,7 +1,14 @@
 var swiper = new Swiper('.swiper-container', {
   direction: 'vertical',
+  // 缩放 zoom
+  // zoom: true,
+  spaceBetween: 30,
+  mousewheel: true,
+  loop: true,
+  effect: 'fade',
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
+  }
+
 });
