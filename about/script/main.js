0ad5758d7efd1a58e9b932823154cac8fbaba4ee
arley <34805850@qq.com>
2018-09-30 15:45:10 +0800

    Site updated: 2018-09-30 15:45:06

--- about/script/main.js
+++ about/script/main.js
@@ -1,14 +1,37 @@
-var swiper = new Swiper('.swiper-container', {
-  direction: 'vertical',
-  // 缩放 zoom
-  // zoom: true,
-  spaceBetween: 30,
-  mousewheel: true,
-  loop: true,
-  effect: 'fade',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  }
+window.onload = function () {
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    // 缩放 zoom
+    // zoom: true,
+    spaceBetween: 30,
+    mousewheel: true,
+    loop: true,
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  
+  });
+  
+  
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
 
-});
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+}
